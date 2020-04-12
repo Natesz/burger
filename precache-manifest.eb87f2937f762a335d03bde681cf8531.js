ab948c29fcc78373880649788dbf67b634836ad2
Natesz <natesz777@yahoo.com>
2020-04-12 22:29:39 +0200

    Updates

---
+++ precache-manifest.eb87f2937f762a335d03bde681cf8531.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3cec2e369824b0d82587cebaac47baf",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "992a4182bc4cbf605db7",
+    "url": "/burger/static/css/main.a753c83e.chunk.css"
+  },
+  {
+    "revision": "aac3cad2701fe39d4563",
+    "url": "/burger/static/js/2.89e3bffe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/burger/static/js/2.89e3bffe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "992a4182bc4cbf605db7",
+    "url": "/burger/static/js/main.092058a7.chunk.js"
+  },
+  {
+    "revision": "b2598febe57dc9942a86",
+    "url": "/burger/static/js/runtime-main.b8c5942e.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/original.b8503d26.png"
+  }
+]);
